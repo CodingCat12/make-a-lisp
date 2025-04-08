c005e8dfa5cc3fcfefba2eed9576b8cff75f5844
Luuk Machiesle <me@gmail.com#>
2025-04-08 11:18:51 +0200

    Use flake-parts

--- flake.nix
+++ flake.nix
@@ -2,26 +2,28 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; 
+    fenix.url = "github:nix-community/fenix";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    fenix,
-    ...
-  }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      overlays = [fenix.overlays.default];
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {
+        config,
+        system,
+        ...
+      }: let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            inputs.fenix.overlays.default
+          ];
+        };
+      in {
+        packages.default = pkgs.callPackage ./nix/default.nix {inherit pkgs;};
+        devShells.default = pkgs.callPackage ./nix/shell.nix {inherit pkgs;};
+      };
     };
-  in {
-    packages.${system}.default = pkgs.callPackage ./nix/default.nix {inherit pkgs;};
-    devShells."${system}".default = pkgs.callPackage ./nix/shell.nix {inherit pkgs;};
-  };
 }
