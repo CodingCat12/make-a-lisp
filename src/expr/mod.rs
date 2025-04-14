cad3c2ed078a5b50d5c6b7449778d0842439e34f
luukm <luuk.machielse@gmail.com>
2025-04-14 18:23:40 +0200

    Only implement the `Expr` trait when necessary

--- src/expr/mod.rs
+++ src/expr/mod.rs
@@ -4,12 +4,19 @@ mod tests;
 
 use std::fmt::Debug;
 
-pub trait Expr<T: Expr<T>>: Debug {
+pub trait Expr<T>: Debug {
     fn eval(&self) -> T;
 }
 
-impl<T: Clone + Debug> Expr<T> for T {
-    fn eval(&self) -> Self {
-        self.clone()
-    }
+macro_rules! expr_impl {
+    // stolen from std::ops::Add (https://doc.rust-lang.org/src/core/ops/arith.rs.html#96)
+    ($($t:ty)*) => ($(
+        impl Expr<Self> for $t {
+            fn eval(&self) -> Self {
+                self.clone()
+            }
+        }
+    )*)
 }
+
+expr_impl! { i32 bool f64 String }
