b09e0d3c956decbee23e68e77e99138ae016f9cd
luukm <luuk.machielse@gmail.com>
2025-04-14 17:58:22 +0200

    Removed unused types from `expr` module

--- src/expr/mod.rs
+++ src/expr/mod.rs
@@ -13,19 +13,3 @@ impl<T: Clone + Debug> Expr<T> for T {
         self.clone()
     }
 }
-
-#[derive(Debug)]
-pub struct Overwrite<D: Expr<D> + 'static, T: Expr<T>> {
-    pub function: Box<dyn Expr<D>>,
-    pub value: Box<dyn Expr<T>>,
-}
-
-impl<D: Expr<D>, T: Expr<T> + Clone> Expr<T> for Overwrite<D, T> {
-    fn eval(&self) -> T {
-        self.function.eval();
-        self.value.eval()
-    }
-}
-
-pub struct Null;
-pub type Ignore<T> = Overwrite<T, Null>;
